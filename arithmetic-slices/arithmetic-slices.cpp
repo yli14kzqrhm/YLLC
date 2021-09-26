3f3c10847bf437a872aa2483c3837bab676fd884
yli14kzqrhm <91082963+yli14kzqrhm@users.noreply.github.com>
2021-09-26 09:57:20 -0400

    Time: 4 ms (55.21%), Space: 7.3 MB (88.15%) - LeetHub

---
+++ arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0, ans = 0;
+        
+        for (int i = 2; i < n; i++) {
+            if ((nums[i] - nums[i-1]) == (nums[i-1] - nums[i-2])) {
+                count++;
+            } else {
+                count = 0;
+            }
+            
+            ans += count;
+        }
+        
+        return ans;
+    }
+};
